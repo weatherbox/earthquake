2c980112ee11da68c8afc0ce1e48b63666a9e8e5
tattii <yuta.tatti@gmail.com>
2018-06-18 03:00:38 +0900

    init

---
+++ database/scrape.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+
+def main():
+    url = 'http://www.data.jma.go.jp/svd/eqdb/data/shindo/index.php'
+    res = requests.post(url, {
+
+    })
+    print(res.text)
+
+if __name__ == '__main__':
+    main()
+
+    print(res.text)
+
+if __name__ == '__main__':
+    main()
+
